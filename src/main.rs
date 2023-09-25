0605780c932e69ef6d2c73ac01c3d99074d3635c
Meek Msaki <meek10x@gmail.com>
2023-09-25 03:38:29 -0500

    fix: separate app logic

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use yew::prelude::*;
+use mod;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter +1;
-            counter.set(value);
-        }
-
-    };
-
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
-}
+use app::App;
 
 fn main() {
     yew::Renderer::<App>::new().render();
